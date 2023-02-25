5d52e821be1db3c4a892e4d7f36b237b49f17577
akkiam2003 <126328049+akkiam2003@users.noreply.github.com>
2023-02-25 13:39:32 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          The app does not collect any user data. No data is shared with third party companies or organizations. The app does not share user data with other companies or organizations.
+          The app does not collect any user data.
+    No data is shared with third party companies or organizations.
+    The app does not share user data with other companies or organizations.
         </p>
       </main>
 
