a63b7dc2ea346f5e5f194efb8633511e579b45f3
akkiam2003 <126328049+akkiam2003@users.noreply.github.com>
2023-02-25 13:09:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,8 @@ export default function Home() {
         <Header title="Welcome to my GameLearnAmharicAlphabet app!" />
         <p className="description">
           Keine Datenerfassung angegeben Der Entwickler gibt an, dass diese App keine Nutzerdaten erfasst
+    Wir benötigen den Zugriff auf den Standort Ihres Gerätes. Bei einer Anfrage erheben wir Ihren aktuellen Standort über GPS, um Ihnen schnell Informationen zu Ihrer unmittelbaren Umgebung geben zu können. Daten zu Ihrem Standort werden nur für die Bearbeitung Ihrer Anfrage genutzt. Die Übertragung Ihrer Standortdaten erfolgt über eine verschlüsselte Verbindung.
+    Ihre Standortdaten werden nach Beendigung Ihrer Anfrage anonymisiert und zur Verbesserung unseres Services statistisch ausgewertet.
         </p>
       </main>
 
